5075bfded0ce0112f0565ea5905dba63389336bc
vmaximussla <163418677+vmaximussla@users.noreply.github.com>
2024-03-15 10:06:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://upload.wikimedia.org/wikipedia/en/4/48/Ultrakill_cover.png";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn14OM88p2ygun240XXfGB6bESN5D65iPdew&usqp=CAU";
+}
+
